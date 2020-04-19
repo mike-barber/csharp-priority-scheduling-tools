919b429ba8a2979baf807fd4f65a425b414c3e95
Michael Barber <60610888+mike-barber@users.noreply.github.com>
2020-04-19 10:25:58 +0100

    Corrected bug in Future and added repeated-invocation guard

--- src/PriorityDemandScheduler/Future.cs
+++ src/PriorityDemandScheduler/Future.cs
@@ -17,7 +17,7 @@ namespace PriorityDemandScheduler
         private readonly Func<T> _function;
         private readonly CancellationToken _cancellationToken;
         private readonly TaskCompletionSource<T> _completionSource;
-
+        private int _runCount = 0;
 
         public Future(Func<T> function, CancellationToken cancellationToken)
         {
@@ -29,9 +29,18 @@ namespace PriorityDemandScheduler
         // run synchronously now
         public override void Run()
         {
+            // check for repeated invocation
+            if (Interlocked.Increment(ref _runCount) != 1)
+            {
+                throw new InvalidOperationException("Cannot execute future more than once");
+            }
+
             // check if already has cancellation requested
             if (_cancellationToken.IsCancellationRequested)
+            {
                 _completionSource.SetCanceled();
+                return;
+            }
 
             // otherwise, attempt to run and record exception if it occurs
             try
