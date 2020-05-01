b3d977fd87812836cafa38b92c80073376d59c57
Michael Barber <60610888+mike-barber@users.noreply.github.com>
2020-05-01 17:04:58 +0100

    added server gc

--- src/PrioritySchedulingTools.Benchmark/Program.cs
+++ src/PrioritySchedulingTools.Benchmark/Program.cs
@@ -9,8 +9,13 @@ namespace PrioritySchedulingTools.Benchmark
     {
         static void Main(string[] args)
         {
+            // in-process
             var job = Job.Default
                 .WithToolchain(BenchmarkDotNet.Toolchains.InProcess.NoEmit.InProcessNoEmitToolchain.Instance);
+
+            // usual config, but with GcServer enabled.
+            //var job = Job.Default
+            //    .WithGcServer(true);
 
             var config = DefaultConfig.Instance
                 .AddJob(job);
